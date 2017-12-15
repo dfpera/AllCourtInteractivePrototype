6d936bed141f268ec1a60ef335b9a8f28cdc61de
Daniele Perazzolo <dfpera@gmail.com>
2017-12-14 18:42:06 -0800

    Progress on results page, mostly adding scores need to style.

--- js/results.js
+++ js/results.js
@@ -1,17 +1,21 @@
 $(function() {
-  $('.resultsSet-One .resultsGame').hide();
-  $('.resultsPoint').hide();
+  $('.resultsSet_content').hide();
+  $('.resultsGame_content').hide();
 
-  $('.resultsSet-One').on('click', function() {
-    $(this).find('.resultsGame').toggle('slide', {direction: 'up'}, 500);
+  $('.resultsSet').on('click', function() {
+    $(this).find('.resultsSet_content').toggle('slide', {direction: 'up'}, 500);
     $(this).find('.resultsSet_title .icon').toggleClass('icon-downV');
     $(this).find('.resultsSet_title .icon').toggleClass('icon-upV');
   });
 
-  $('.resultsSet-One .resultsGame').on('click', function(event) {
+  $('.resultsSet .resultsGame').on('click', function(event) {
     event.stopPropagation();
-    $(this).find('.resultsPoint').toggle('slide', {direction: 'up'}, 500);
-    $(this).find('.resultsGame_title .icon').toggleClass('icon-plus');
-    $(this).find('.resultsGame_title .icon').toggleClass('icon-minus');
+    $(this).find('.resultsGame_content').toggle('slide', {direction: 'up'}, 500);
+    $(this).find('.resultsGame_title .icon').toggleClass('icon-downV');
+    $(this).find('.resultsGame_title .icon').toggleClass('icon-upV');
+  });
+
+  $('.resultsSet .resultsGame .resultsPoint').on('click', function(event) {
+    event.stopPropagation();
   });
 });
